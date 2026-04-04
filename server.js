c7ca50541886e92bdcd2db61a29db968378fa361
emmasametz <esametz2003@gmail.com>
2026-04-03 19:16:10 -0500

    Fix port for Render

--- server.js
+++ server.js
@@ -32,4 +32,5 @@ app.post("/feedback", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
